3a40b1bba9699afa658b006e4e515d3498e39f99
bardabarda228 <bardabarda228@gmail.com>
2021-12-05 14:21:54 +0500

    Controllers logic in progress

---
+++ CryptolioAPI/Controllers/PortfolioController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CryptolioAPI.Dtos;
+using CryptolioAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace CryptolioAPI.Controllers
+{
+    [ApiController]
+    [Route("portfolio")]
+    public class PortfolioController : ControllerBase
+    {
+        private ApplicationContext db;
+
+        public PortfolioController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        /// <summary>
+        /// Получить данные о портфелях пользователя
+        /// </summary>
+        /// <param name="user_id"></param>
+        /// <returns></returns>
+        [HttpGet("user_portfolios")]
+        public ActionResult<IEnumerable<PortfolioDto>>
+            GetUsersPortfolio(int user_id) //user_id - temporary solution until jwt will be included
+        {
+            return db.Portfolios.Where(portfolio => portfolio.UserId == user_id).Include(x => x.User).Select(portfolio => portfolio.AsDto())
+                .ToList();
+        }
+
+        /// <summary>
+        /// Создать новый портфель
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="user_id"></param>
+        /// <returns></returns>
+        [HttpPost("create")]
+        public ActionResult<PortfolioDto>
+            CreatePortfolio(string name, int user_id) //user_id - temporary solution until jwt will be included
+        {
+            if (db.Portfolios.Include(x => x.User).SingleOrDefault(portfolio =>
+                portfolio.User.Id == user_id && portfolio.PortfolioName == name) is not null)
+            {
+                return Conflict("Portfolio with given name already exists");
+            }
+
+            var portfolio = new Portfolio()
+            {
+                UserId = user_id,
+                PortfolioName = name
+            };
+            db.Portfolios.Add(portfolio);
+            db.SaveChanges();
+
+            portfolio = db.Portfolios.Include(x => x.User).SingleOrDefault(x => x.Id == portfolio.Id);
+            return portfolio.AsDto();
+        }
+
+        /// <summary>
+        /// Удалить портфель
+        /// </summary>
+        /// <param name="portfolio_id"></param>
+        /// <param name="user_id"></param>
+        /// <returns></returns>
+        [HttpPost("delete")]
+        public ActionResult
+            DeletePortfolio(int portfolio_id, int user_id) ////user_id - temporary solution until jwt will be included
+        {
+            var portfolio = db.Portfolios.SingleOrDefault(x => x.Id == portfolio_id && x.UserId == user_id);
+            if (portfolio is null)
+            {
+                return Conflict("Error with deleting portfolio");
+            }
+
+            db.Portfolios.Remove(portfolio);
+            return Ok("Portfolio Deleted");
+        }
+
+        /// <summary>
+        /// Получить записи в определенном портфеле
+        /// </summary>
+        /// <param name="portfolio_id"></param>
+        /// <returns></returns>
+        [HttpGet("{portfolio_id}")]
+        public ActionResult<IEnumerable<PortfolioRecordDto>> GetPortfolioInfo(int portfolio_id)
+        {
+            var portfolio = db.Portfolios.Include(x => x.User).SingleOrDefault(x => x.Id == portfolio_id);
+            if (portfolio is null)
+            {
+                return Conflict("Error with getting portfolio");
+            }
+
+            var records = db.PortfolioRecords.Include(x => x.Portfolio).Include(x => x.Coin)
+                .Where(x => x.Portfolio == portfolio).ToList();
+            return records.Select(x => x.AsDto()).ToList();
+        }
+
+        /// <summary>
+        /// Добавить запись в портфель
+        /// </summary>
+        /// <param name="portfolio_id"></param>
+        /// <param name="coin_id"></param>
+        /// <param name="buytime"></param>
+        /// <param name="buyprice"></param>
+        /// <param name="amount"></param>
+        /// <param name="note"></param>
+        /// <returns></returns>
+        [HttpPost("{portfolio_id}/add")]
+        public ActionResult AddRecordToPortfolio(int portfolio_id, int coin_id, int buytime, int buyprice, int amount,
+            string note = "")
+        {
+            var portfolio = db.Portfolios.FirstOrDefault(x => x.Id == portfolio_id);
+            if (portfolio is null)
+            {
+                return Conflict("Error with getting portfolio");
+            }
+
+            db.PortfolioRecords.Add(new PortfolioRecord()
+            {
+                Amount = amount,
+                BuyPrice = buyprice,
+                BuyTime = new DateTime(1970,1,1,0,0,0,0).AddSeconds(buytime),
+                CoinId = coin_id,
+                Notes = note,
+                PortfolioId = portfolio_id
+            });
+            db.SaveChanges();
+            return Ok("Record added");
+        }
+
+        /// <summary>
+        /// Удалить запись из портфеля
+        /// </summary>
+        /// <param name="record_id"></param>
+        /// <returns></returns>
+        [HttpPost("remove_record/{record_id}")]
+        public ActionResult RemoveRecordFromPortfolio(int record_id) // check next if record not assigned with authorized user
+        {
+            var record = db.PortfolioRecords.SingleOrDefault(x => x.Id == record_id);
+            if (record is null)
+            {
+                return Conflict("Error with removing record");
+            }
+
+            db.PortfolioRecords.Remove(record);
+            return Ok("Record removed");
+        }
+    }
+}
---
+++ CryptolioAPI/Controllers/UserController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CryptolioAPI.Dtos;
+using CryptolioAPI.Models;
+using CryptolioAPI.Models.Additional;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using static System.Int32;
+
+namespace CryptolioAPI.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private ApplicationContext db;
+        private IConfiguration _config;
+        public UserController(ApplicationContext context, IConfiguration configuration)
+        {
+            db = context;
+            _config = configuration;
+        }
+
+        /// <summary>
+        /// Получить список всех пользователей
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("all")]
+        public ActionResult<IEnumerable<UserDto>> GetUsers()
+        {
+            return db.Users.Select(item => item.AsDto()).ToList();
+        }
+
+        /// <summary>
+        /// Получить данные пользователя
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("me")]
+        [Authorize]
+        public ActionResult<UserDto> GetUser()
+        {
+            var currentUser = GetCurrentUser();
+            if (currentUser is null)
+            {
+                return Conflict("Cant resolve user from jwt");
+            }
+            var user = db.Users.SingleOrDefault(item => item.Id == currentUser.Id);
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return user.AsDto();
+        }
+
+        /// <summary>
+        /// Регистрация
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="nickname"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public ActionResult<UserSettingsDto> Register(string email, string nickname, string password)
+        {
+            if (db.Users.SingleOrDefault(item => item.Email == email) != null)
+            {
+                return Conflict("Email already exists");
+            }
+            if (db.Users.SingleOrDefault(item => item.Nickname == nickname) != null)
+            {
+                return Conflict("Nickname already exists");
+            }
+
+            User user = new User()
+            {
+                Email = email,
+                Nickname = nickname,
+                Password = password,
+                CreatedOn = DateTime.UtcNow
+            };
+            db.Users.Add(user);
+            db.SaveChanges();
+            return user.AsDtoSettings();
+        }
+
+        /// <summary>
+        /// Авторизация
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public ActionResult<UserSettingsDto> Authorize(string email, string password)
+        {
+            var user = db.Users.SingleOrDefault(user => user.Email == email && user.Password == password);
+            if (user == null)
+            {
+                return Conflict("Wrong Credentials");
+            }
+
+            var token = GenerateJwtToken(user);
+            //return user.AsDtoSettings();
+            return Ok(token);
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("user_id", user.Id.ToString()),
+                new Claim("email", user.Email),
+                new Claim("nickname", user.Nickname)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(1440),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserJwt GetCurrentUser()
+        {
+            if (HttpContext.User.Identity is ClaimsIdentity identity)
+            {
+                var userClaims = identity.Claims;
+                return new UserJwt()
+                {
+                    Email = userClaims.SingleOrDefault(x => x.Type == "email")?.Value,
+                    Id = Parse(userClaims.SingleOrDefault(x => x.Type == "user_id")?.Value),
+                    Nickname = userClaims.SingleOrDefault(x => x.Type == "nickname")?.Value
+                };
+            }
+
+            return null;
+        }
+    }
+}
