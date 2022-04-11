5a3346c227766045e2af76217ee32661498185c2
trum <bardabarda228@gmail.com>
2022-04-11 13:28:24 +0500

    [+] PortfolioUpdate class for requests

---
+++ CryptolioAPI/Models/Additional/PortfolioUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptolioAPI.Models.Additional
+{
+    public class PortfolioUpdate
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        [MinLength(1)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+    }
+}
