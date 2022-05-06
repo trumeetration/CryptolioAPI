647dfe5ef3762dd8d789b9d27ad2c5be504088bf
trum <bardabarda228@gmail.com>
2022-05-06 17:41:21 +0500

    Added status flag to record

--- CryptolioAPI/Models/PortfolioRecord.cs
+++ CryptolioAPI/Models/PortfolioRecord.cs
@@ -11,10 +11,11 @@ namespace CryptolioAPI.Models
         [Column("portfolio_id")] public int PortfolioId { get; set; }
         public Portfolio Portfolio { get; set; }
         [Column("coin_id")] public string CoinId { get; set; }
-        [Column("buy_time")] public DateTime BuyTime { get; set; }
-        [Column("buy_price")] public double BuyPrice { get; set; }
+        [Column("tx_time")] public DateTime TxTime { get; set; }
+        [Column("tx_price")] public double TxPrice { get; set; }
         [Column("amount")] public double Amount { get; set; }
         [Column("notes")] public string Notes { get; set; }
         [Column("record_type")] public string RecordType { get; set; }
+        [Column("status")] public string Status { get; set; }
     }
 }
