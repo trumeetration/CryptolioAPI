fed83f43d7f05d8affaa7ba89e08d7aa135b13ec
trum <bardabarda228@gmail.com>
2022-05-06 17:41:01 +0500

    Refactored

--- CryptolioAPI/Dtos/PortfolioDto.cs
+++ CryptolioAPI/Dtos/PortfolioDto.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
-using CryptolioAPI.Models;
-
+﻿
 namespace CryptolioAPI.Dtos
 {
     public class PortfolioDto
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        //public UserDto User { get; set; }
         public string PortfolioName { get; set; }
     }
 }
