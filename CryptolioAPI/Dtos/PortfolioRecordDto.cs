9f71e895acc1e0c52489ed9342e5ead42b5df4c1
trum <bardabarda228@gmail.com>
2022-05-06 17:42:00 +0500

    Added status flag to record

--- CryptolioAPI/Dtos/PortfolioRecordDto.cs
+++ CryptolioAPI/Dtos/PortfolioRecordDto.cs
@@ -7,10 +7,11 @@ namespace CryptolioAPI.Dtos
         public int Id { get; set; }
         public PortfolioDto Portfolio { get; set; }
         public string CoinId { get; set; }
-        public DateTime BuyTime { get; set; }
-        public double BuyPrice { get; set; }
+        public DateTime TxTime { get; set; }
+        public double TxPrice { get; set; }
         public double Amount { get; set; }
         public string Notes { get; set; }
         public string RecordType { get; set; }
+        public string Status { get; set; }
     }
 }
