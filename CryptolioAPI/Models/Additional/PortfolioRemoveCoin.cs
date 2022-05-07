049932d5fda71ef77199ef3fff430667d6623f66
trum <bardabarda228@gmail.com>
2022-05-07 21:27:51 +0500

    [+] class (json body) for "remove_coin" request

---
+++ CryptolioAPI/Models/Additional/PortfolioRemoveCoin.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptolioAPI.Models.Additional
+{
+    public class PortfolioRemoveCoin
+    {
+        [Required] public int PortfolioId { get; set; }
+        [Required] public string CoinId { get; set; }
+    }
+}
