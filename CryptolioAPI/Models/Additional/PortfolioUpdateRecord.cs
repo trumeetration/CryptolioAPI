340157ef20dad7b6f1e6218d51b03b90a40f3a05
trum <bardabarda228@gmail.com>
2022-05-15 23:13:26 +0500

    [+] class for portfolio record update request

---
+++ CryptolioAPI/Models/Additional/PortfolioUpdateRecord.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptolioAPI.Models.Additional
+{
+    public class PortfolioUpdateRecord
+    {
+        [Required] 
+        public int RecordId { get; set; }
+        [Required] 
+        public string CoinId { get; set; }
+        [Required]
+        [Timestamp]
+        public int TxTime { get; set; }
+        [Required]
+        [Range(0, 9999999999)]
+        public double TxPrice { get; set; }
+        [Required]
+        [Range(0, 9999999999)]
+        public double Amount { get; set; }
+        [MaxLength(500)]
+        public string Notes { get; set; }
+
+        [Required] public string RecordType { get; set; }
+    }
+}
