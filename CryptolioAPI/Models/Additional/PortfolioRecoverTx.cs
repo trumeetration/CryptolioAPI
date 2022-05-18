f5697f66d22d2b8a10dceefa5ffb392dfc46ef2b
trum <bardabarda228@gmail.com>
2022-05-18 16:49:23 +0500

    [+] Recover transaction

---
+++ CryptolioAPI/Models/Additional/PortfolioRecoverTx.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptolioAPI.Models.Additional
+{
+    public class PortfolioRecoverTx
+    {
+        [Required]
+        public int RecordId { get; set; }
+    }
+}
